e47806a0a2fd5d7b9ab2add1ec096901d0319106
dmitru4ok <mobilokh4566@gmail.com>
2023-02-20 17:38:03 +0200

    Added task 1

--- Lab1_Dmytruk/Program.cs
+++ Lab1_Dmytruk/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+
 
 namespace Lab1_Dmytruk
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        static void Main()
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Task1();
+            Console.ReadLine();
+        }
+
+        static void Task1()
+        {
+            Console.Write("Введіть вісімкове число: ");
+            string input = Console.ReadLine();
+            
+            int result = 0;
+            int PowVariable = 1;
+            bool InputIsCorrect = true;
+            for (int i = input.Length - 1; i>=0; --i)
+            {
+                if (input[i] >= '0' && input[i] <= '7')
+                {
+                    result += PowVariable * Convert.ToInt32(input[i] - '0'); //або можна input[i].ToString()
+                    PowVariable *= 8;
+                } 
+                else
+                {
+                    InputIsCorrect = false;
+                    break; 
+                }
+            }
+
+            if (InputIsCorrect)
+            {
+                Console.WriteLine($"Число {input} у вісімковiй рівне {Convert.ToUInt32(input, 8)} у десятковій");
+                Console.WriteLine($"Число {input} у вісімковiй рівне {result} у десятковій");
+            }
+            else
+            {
+                Console.WriteLine($"{input} не є вісімковим числом!");
+            }
         }
     }
 }
